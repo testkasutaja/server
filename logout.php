085a0511ed64ef6cb7063a49be7d2d3483b9ba00
testkasutaja <39335682+testkasutaja@users.noreply.github.com>
2018-09-06 09:31:57 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,9 @@
 <?php
-// Initialize the session
 session_start();
- 
-// Unset all of the session variables
-$_SESSION = array();
- 
-// Destroy the session.
-session_destroy();
- 
-// Redirect to login page
-header("location: login.php");
-exit;
+// Destroying All Sessions
+if(session_destroy())
+{
+// Redirecting To Home Page
+header("Location: login.php");
+}
 ?>
